51f16c84e6a25f1325e3d40128affa8d26a1e32f
SmallWood-D <shay.gozan@gmail.com>
2021-09-07 00:48:22 +0300

    update loader

--- resources/loader.py
+++ resources/loader.py
@@ -1,9 +1,46 @@
 import os.path as path
 import json
+from unicodedata import numeric
+from fractions import Fraction
 
 from pymongo import MongoClient
 
 mongoClient = MongoClient("mongodb://localhost:27017/?retryWrites=true&w=majority")
+
+
+def convert_quantity(c):
+    c = str(c)
+    quantity = '-1'
+    try:
+        if 'to' in c:
+            [low, high] = c.split('to')
+            low = convert_unicode_numeric(low.strip())
+            high = convert_unicode_numeric(high.strip())
+            quantity = f'{low} - {high}'
+        elif 'or' in c:
+            [low, high] = c.split('or')
+            low = convert_unicode_numeric(low.strip())
+            high = convert_unicode_numeric(high.strip())
+            quantity = f'{low} - {high}'
+        elif '/' in c:
+            quantity = float(sum(Fraction(s) for s in c.split()))
+        else:
+            quantity = convert_unicode_numeric(c)
+    except Exception as e:
+        print(f'Error {e}')
+    return quantity
+
+
+def convert_unicode_numeric(c):
+    if len(c) == 1:
+        v = numeric(c)
+    elif c[-1].isdigit():
+        # normal number, ending in [0-9]
+        v = float(c)
+    else:
+        # Assume the last character is a vulgar fraction
+        v = float(c[:-1]) + numeric(c[-1])
+    return v
 
 
 def read_write_recipes(file_path):
@@ -13,7 +50,7 @@ def read_write_recipes(file_path):
     db = mongoClient.recepies
     for url, data in recipes.items():
         print(url)
-        ingredients_dict = [{'name':i.replace(".", " "),  'quantity': str(c)} for c, i in data['ingredients']]
+        ingredients_dict = [{'name':i.replace(".", " "),  'quantity': convert_quantity(c)} for c, i in data['ingredients']]
         db.recepies.insert_one({"name": url, "ingredients": ingredients_dict, "instructions": data['instructions']})
 
 
