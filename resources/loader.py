9553121829a213fda31a79a1fda1f7d5a5de597e
SmallWood-D <shay.gozan@gmail.com>
2021-09-28 15:57:43 +0300

    update loader script

--- resources/loader.py
+++ resources/loader.py
@@ -1,16 +1,21 @@
-import os.path as path
 import json
+import re
 from unicodedata import numeric
 from fractions import Fraction
-
 from pymongo import MongoClient
 
+DB_NAME = "NightChef"
 mongoClient = MongoClient("mongodb://localhost:27017/?retryWrites=true&w=majority")
 
 
-def convert_quantity(c):
+def convert_quantity(c: str) -> float:
+    """
+    Covert stringify ingredientes quantity.
+    :param c: quantity
+    :return: float value or better string representation or -1 if the conversion failed.
+    """
     c = str(c)
-    quantity = '-1'
+    quantity = -1.0
     try:
         if 'to' in c:
             [low, high] = c.split('to')
@@ -31,7 +36,12 @@ def convert_quantity(c):
     return quantity
 
 
-def convert_unicode_numeric(c):
+def convert_unicode_numeric(c: str) -> float:
+    """
+    Covert unicode fraction from string to float.
+    :param c: unicode character
+    :return: float value
+    """
     if len(c) == 1:
         v = numeric(c)
     elif c[-1].isdigit():
@@ -43,15 +53,69 @@ def convert_unicode_numeric(c):
     return v
 
 
-def read_write_recipes(file_path):
+def upload_recipes(file_path: str) -> None:
+    """
+    Upload all the recipes
+    :param file_path: source file path
+    :return: None
+    """
     with open(file_path, 'r', encoding='utf-8') as infile:
         recipes = json.load(infile)
 
-    db = mongoClient.recepies
+    db = mongoClient[DB_NAME]
     for url, data in recipes.items():
-        print(url)
+
         ingredients_dict = [{'name':i.replace(".", " "),  'quantity': convert_quantity(c)} for c, i in data['ingredients']]
         db.recepies.insert_one({"name": url, "ingredients": ingredients_dict, "instructions": data['instructions']})
 
 
-read_write_recipes('./sample.json')
+def upload_ingredients(file_path: str) -> None:
+    """
+    Upload all the ingredients
+    :param file_path: source file path
+    :return:  None
+    """
+    with open(file_path, 'r', encoding='utf-8') as infile:
+        db = mongoClient[DB_NAME]
+        for line in infile:
+            db.ingredients.insert_one({"name": line.strip()})
+
+
+def create_ingredients_keys() -> None:
+    db = mongoClient[DB_NAME]
+    x = db.ingredients.find({}, {"_id": 0})
+    re_ingredients = re.compile('(' + '|'.join(map(lambda e : e['name'] + 'e?s?', x)) + ')', flags=re.IGNORECASE|re.ASCII)
+    # print(re_ingredients)
+    lk = '(' + '|'.join(map(lambda e : e['name'] + 'e?s?', x)) + ')'
+    # db.recepies.update_many({}, [{"$set": {"modified": "$$NOW"}}])
+    y = db.recepies.aggregate([
+        {
+            "$addFields": {
+                "keys": {
+                    "$map": {
+                        "input": "$ingredients",
+                        "as": "grade",
+                        "in": {"$regexFind": {"input": "$$grade.name", "regex": re_ingredients}}
+                    }
+                }
+            }
+        },
+        {
+            "$addFields": {
+                "keys": "$keys.match"
+            }
+        },
+        {
+            "$project": {
+                "keys": 1,
+                "name": 1,
+                "ingredients": 1,
+                "_id": 0
+            }
+        },
+        {"$out":{"db": "NightChef", "coll": "data"}}
+    ])
+
+upload_recipes('./sample - Copy.json')
+upload_ingredients('./Food.csv')
+create_ingredients_keys()
